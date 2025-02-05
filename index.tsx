ba0c609445ee318f22e93cff0aaee35f5880a08a
sadan <117494111+sadan4@users.noreply.github.com>
2025-02-05 01:50:50 -0500

    start plugin

--- index.tsx
+++ index.tsx
@@ -1,12 +1,64 @@
-import definePlugin from "@utils/types";
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2025 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
 
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+import { filters, waitFor } from "@webpack";
+
+export const Avatar = waitFor(filters.componentByCode(".size-1.375*"), m => {
+    Object.defineProperty(m, "name", { value: "Avatar" });
+});
 export default definePlugin({
     name: "ComponentDemangler",
     description: "",
-    authors: [
+    authors: [Devs.sadan],
+
+    patches: [
         {
-            name: "sadan",
-            id: 521819891141967883n
-        }
+            find: "FocusRing was given",
+            replacement: {
+                match: /function (\i)\(\i\)\{.{0,300}FocusRing was/,
+                replace: (_, funcIdent) => {
+                    return `Object.defineProperty(${funcIdent},"name", {value: "FocusRing"});${_}`;
+                }
+            }
+        },
+        {
+            find: "20+20*Math.random()",
+            replacement: {
+                match: /class \i extends.+?{/,
+                replace: _ => {
+                    return `${_}static displayName = "Popout";`;
+                }
+            }
+        },
+        {
+            find: "this.context?this.renderNonInteractive():",
+            replacement: {
+                match: /class \i extends.+?{/,
+                replace: _ => {
+                    return `${_}static displayName = "Clickable";`;
+                }
+            }
+        },
+        {
+            find: ".tooltipTop,bottom:",
+            // Tooltip is the class
+            // TooltipContainer is the function
+            replacement: [
+                {
+                    match: /class \i extends.+?\{/,
+                    replace: "$&static displayName = \"Tooltip\";"
+                },
+                {
+                    match: /let (\i)=\i=>\{.{0,200}="div"/,
+                    replace: "setTimeout(() => Object.defineProperty($1, \"name\", {value: \"TooltipContainer\"}));$&"
+                }
+            ]
+        },
     ]
-})
+}
+);
